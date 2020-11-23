f4574a1e0870789410f05351b0a361dc6545a559
mishajib <mishajib222@gmail.com>
2020-11-23 23:12:55 +0600

    delete pipe

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -5,7 +5,7 @@
                 wire:keydown.arrow-down="decrement({{ $step }})" wire:model.lazy="step" class="form-control">
         </p>
         <button wire:click="decrement({{ $step }})" class="btn btn-secondary">Minus -</button>
-        |<button wire:click="increment({{ $step }})" class="btn btn-primary">Plus +</button>
+        <button wire:click="increment({{ $step }})" class="btn btn-primary">Plus +</button>
         <h1 class="h1">{{ $count }}</h1>
     </div>
 </div>
